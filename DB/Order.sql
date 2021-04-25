3193482cd984c52f365ad065617a7e30c8394b98
RusiraPathum <48381430+RusiraPathum@users.noreply.github.com>
2021-04-25 22:08:17 +0530

    Create Order.sql
    order DB file add

---
+++ DB/Order.sql
@@ -0,0 +1,24 @@
+===Database gadgetbadget
+
+== Table structure for table ordertable
+
+|------
+|Column|Type|Null|Default
+|------
+|//**orderID**//|int(11)|No|
+|orderCode|varchar(15)|No|
+|orderType|varchar(50)|No|
+|customerName|varchar(50)|No|
+|customerContact|varchar(50)|No|
+|totalAmount|double(20,3)|No|
+|cardNo|int(11)|No|
+|cvvNo|int(11)|No|
+== Dumping data for table ordertable
+
+|1001|100|zip|janaka|714563242|50000.000|444444|678
+|1002|101|zip|anuka|776675444|30000.000|111111|123
+|1003|102|zip|asanath|776554778|40000.000|222222|999
+|1004|103|Zip|suranga|751119812|1500.000|777777|432
+|1005|104|Zip|suranga|751119812|1500.000|777777|432
+|1006|105|Zip|jeewa|784147721|18000.000|555555|771
+|1007|106|Zip|prasad|774551472|18000.000|666666|885
