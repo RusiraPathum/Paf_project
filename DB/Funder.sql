8d0ab555239eb745715bed8a7e1b3b3158107482
RusiraPathum <48381430+RusiraPathum@users.noreply.github.com>
2021-04-25 22:09:04 +0530

    Create Funder.sql
    funder DB file add

---
+++ DB/Funder.sql
@@ -0,0 +1,16 @@
+===Database gadgetbadget
+
+== Table structure for table funder
+
+|------
+|Column|Type|Null|Default
+|------
+|//**id**//|int(100)|No|
+|title|varchar(200)|No|
+|content|varchar(200)|No|
+|pdate|varchar(20)|No|
+|ptime|varchar(20)|No|
+== Dumping data for table funder
+
+|1|update|update|today|after
+|3|funder|management|20.04.21|12.08a.m
